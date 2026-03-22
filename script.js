d9711d6066b1a23d768ad0a0d99edfc5da2a410a
sarbaniadhikari <114319923+sarbaniadhikari@users.noreply.github.com>
2026-03-22 11:58:11 -0400

    Added profile, login, and signup page. Created script.js (functionality to be added later)

---
+++ script.js
@@ -0,0 +1,19 @@
+//script.js
+
+// browser page functions
+
+// home page functions
+
+// profile page functions
+
+// login page functions
+function tryLogin(){
+    //add more logic later, redirect to home
+    window.location.href = "home.html";
+}
+
+// signup page functions
+function tryCreateAccount(){
+    //add more logic later, redirect to home
+    window.location.href = "home.html";
+}
